95bb74cfea54dc88839c39f1e214bd38994c86fa
Gunasekaran-Annamalai <gunasekaran021098@gmail.com>
2020-05-25 18:10:32 +0530

    commit

--- game.py
+++ game.py
@@ -1,8 +1,22 @@
 board = ['-', '-', '-', 
         '-', '-', '-', 
         '-', '-', '-']
-def display():
+
+navigator = [1, 2, 3, 
+            4, 5, 6, 
+            7, 8, 9]
+
+
+def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
-display()
+
+
+def display_navigator():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+display_board()
+display_navigator()
