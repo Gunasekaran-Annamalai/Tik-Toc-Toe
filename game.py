37717015195f3930b643d69dc6abc535ddb701ec
Gunasekaran-Annamalai <gunasekaran021098@gmail.com>
2020-05-25 18:32:29 +0530

    commit

--- game.py
+++ game.py
@@ -1,14 +1,28 @@
-board = ['-', '-', '-', 
-        '-', '-', '-', 
-        '-', '-', '-']
+class game_begins:
+    user_x = input('Enter \'X\' name:')
+    user_O = input('Enter \'O\' name:')
 
-def display_board():
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
+    # just for some empty lines
+    for i in range(2):
+        print("")
 
-display_board()
+    print('Game Begins')
+    def game_starts(self):
 
-def navigator():
-    print("Choose any place in between 1-9:")
-navigator()
+        board = ['-', '-', '-', 
+                '-', '-', '-', 
+                '-', '-', '-']
+
+        def display_board():
+            print(board[0] + " | " + board[1] + " | " + board[2])
+            print(board[3] + " | " + board[4] + " | " + board[5])
+            print(board[6] + " | " + board[7] + " | " + board[8])
+
+        display_board()
+
+        def navigator():
+            print("Choose any place in between 1-9:")
+        navigator()
+
+obj = game_begins()
+obj.game_starts()
