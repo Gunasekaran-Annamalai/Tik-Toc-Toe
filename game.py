58f09e64f5d6e88dd8d5d4378e8255bca25eac4a
Gunasekaran-Annamalai <gunasekaran021098@gmail.com>
2020-05-30 16:26:49 +0530

    commit

--- game.py
+++ game.py
@@ -8,41 +8,42 @@ class game_begins:
 
     print('Game Begins\n')
     def game_starts(self):
-        board = ['-', '-', '-', 
+        self.board = ['-', '-', '-', 
                 '-', '-', '-', 
                 '-', '-', '-']
 
-        def display_board():
-            print(board[0] + " | " + board[1] + " | " + board[2])
-            print(board[3] + " | " + board[4] + " | " + board[5])
-            print(board[6] + " | " + board[7] + " | " + board[8])
+    def display_board(self):
+        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
+        print(self.board[3] + " | " + self.board[4] + " | " + self.board[5])
+        print(self.board[6] + " | " + self.board[7] + " | " + self.board[8])
 
-        display_board()
+    def navigator(self):
+        print("Choose any place in between 1-9:")
+        print()
+        print(1, "|", 2, "|", 3)
+        print(4, "|", 5, "|", 6)
+        print(7, "|", 8, "|", 9)
+class game_play(game_begins):
 
-        def navigator():
-            print("Choose any place in between 1-9:")
-        navigator()
+    def player_x(self):
+        print('\'X\' turn...')
+        limit_checker = True
+        while(limit_checker):
+            decision = int(input('Enter any number : '))
+            if(decision <= 9 and decision > 0):
+                limit_checker = False
+            else: print("Error enter correct choice")
 
-        def player_x():
-            print('\'X\' turn...')
-            limit_checker = True
-            while(limit_checker):
-                decision = int(input('Enter any number : '))
-                if(decision <= 9 and decision > 0):
-                    limit_checker = False
-                else: print("Error enter correct choice")
+    def player_o(self):
+        print('\'O\' turn...')
+        limit_checker = True
+        while(limit_checker):
+            decision = int(input('Enter any number : '))
+            if(decision <= 9 and decision > 0):
+                limit_checker = False
+            else: print("Error enter correct choice")
 
-        def player_o():
-            print('\'O\' turn...')
-            limit_checker = True
-            while(limit_checker):
-                decision = int(input('Enter any number : '))
-                if(decision <= 9 and decision > 0):
-                    limit_checker = False
-                else: print("Error enter correct choice")
-        
-        player_x()
-        player_o()
-
-obj = game_begins()
-obj.game_starts()
+gameobj = game_play()
+gameobj.game_starts()
+gameobj.navigator()
+gameobj.display_board()
